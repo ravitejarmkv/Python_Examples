92d47cb83c077dfb7b223f9243a3f23c996ef5ee
[rmkvraviteja] <ravitejarmkv@github.com>
2021-08-23 16:29:48 +0530

    Python Examples(test)

--- Examples/7.py
+++ Examples/7.py
@@ -1,3 +1,15 @@
 """
 Write a program to calculate the number of numerical digits in a string
 """
+
+
+def digits_in_string(text):
+    count = 0
+    for i in text:
+        if i.isdigit():
+            count += 1
+    print(count)
+
+
+text = 'I have 2 apple, 4 oranges and 1 chocolate.'
+digits_in_string(text)
